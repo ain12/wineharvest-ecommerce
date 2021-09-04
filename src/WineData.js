eea253486be96b019d6f2e49e3af5cefb0c3ac72
Ainhoa <ainhoaactic@gmail.com>
2021-09-04 11:25:40 +0200

    basic data for wine products created

---
+++ src/WineData.js
@@ -0,0 +1,63 @@
+const data = {
+    status: "success",
+    items: [
+        {
+            id: 1,
+            title: "Barbera",
+            price: 20,
+            category: "wine",
+            quantity: 1,
+        },
+        {
+            id: 2,
+            title: "Dolcetto ",
+            price: 50,
+            category: "wine",
+            quantity: 1
+        },
+        {
+            id: 3,
+            title: "Charbono ",
+            price: 100,
+            category: "wine",
+            quantity: 1
+        },
+        {
+            id: 4,
+            title: "Carignan",
+            price: 65,
+            category: "wine",
+            quantity: 1
+        },
+        {
+            id: 5,
+            title: "Gamay",
+            price: 200,
+            category: "wine",
+            quantity: 1
+        },
+        {
+            id: 6,
+            title: "Cabernet Franc",
+            price: 130,
+            category: "wine",
+            quantity: 1
+        },
+        {
+            id: 7,
+            title: "Mourvedre",
+            price: 15,
+            category: "wine",
+            quantity: 1
+        },
+        {
+            id: 8,
+            title: "Muscat",
+            price: 90,
+            category: "wine",
+            quantity: 1
+        },
+    ],
+};
+
+export default data;
